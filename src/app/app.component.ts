4d41c7ce4ecbf73a6e64d33fb2b841674d92978c
DarrellSab <darrell74@gmail.com>
2021-07-23 09:49:24 +0300

    Angular examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+
+
   title = 'angular-interview';
+  localNumber: number = 0;
+  // @ts-ignore
+  volunteerForm: FormGroup;
+  number1 = 1;
+  number2 = 2;
+  name = new FormControl('');
+  backgroundToggle: any = false;
+  arr: any = ['apple','pineapple'];
+  objs = [{
+    lastName: 'Smith', age:12, address: [
+      {
+      city: 'Vilnius', street: 'Druskio'
+    },
+      {city: 'Klaipeda', street: 'Baltijos'}]
+  },
+    {lastName: 'Jeremy', age:24, address: [
+    {
+      city: 'Vilnius', street: 'Viesulo'
+    },
+    {city: 'Klaipeda', street: 'Pilies'}]
+}
+
+  ];
+  obj = {
+    firstName: 'John', lastName: 'Blacksmith', address: {
+      city: 'Alytus', street: 'Puntuko'
+    }
+  }
+
+  date: Date = new Date();
+
+
+
+  constructor(private fb:FormBuilder) {
+  }
+  ngOnInit(){
+    this.initializeForm();
+  }
+
+  min(num1:number, num2:number){
+    return Math.min(num1,num2);
+  }
+  sum(num1:number, num2:number){
+    return num1+num2;
+  }
+
+   initializeForm(): void {
+    this.volunteerForm = this.fb.group({
+      firstName: 'Name here',
+      phoneNumber: '',
+    })
+  }
+  onSubmit(): void{
+    console.log(this.volunteerForm);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+import {CounterComponent} from './counter/counter.component';
+import { ClothesComponent } from './clothes/clothes.component';
+import { FormComponent } from './form/form.component';
+import { RegistrationComponent } from './registration/registration.component';
+import {UserRegistrationService} from "./user-registration.service";
+import {HttpClientModule} from "@angular/common/http";
+import { SearchDeleteComponent } from './search-delete/search-delete.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { EditComponent } from './edit/edit.component';
+
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    ClothesComponent,
+    FormComponent,
+    RegistrationComponent,
+    SearchDeleteComponent,
+    NavbarComponent,
+    HomeComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgxPaginationModule
   ],
-  providers: [],
+  providers: [UserRegistrationService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
